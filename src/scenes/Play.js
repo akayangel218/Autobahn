6d4e0f737d5dde01d26b0f69bea4f8053bba1a39
Mateen <maaminia@gmail.com>
2021-07-12 15:01:46 -0700

    added arcade physics to background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,12 +10,15 @@ class Play extends Phaser.Scene {
 
     create() {
         //place tile sprite
-        this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0, 0);
+        //this.background = this.add.sprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0, 0);
+        this.background = this.physics.add.sprite(0, 0, 'background').setOrigin(0);
+        //this.background.setAllowGravity(true);
     }
 
     update() {
-        //this.background.tilePositionY -= 6;
-        //this.background.tilePositionx -= 2;
+        this.background.setDragY(-6);
+        //this.background.setDragX(-6);
+
     }
 
     // helper functions go here
