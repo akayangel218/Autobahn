ab99572da0b619ad63d212034a0ae349d85702a6
Kidus Michael <kimichae@ucsc.edu>
2021-07-12 22:35:09 -0700

    Car code added

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,1 +1,25 @@
 // car goes here
+class Car extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        super.add.existing(this);
+        scene.physics.add.existing(this);
+        this.moveSpeed = 2;
+    }
+
+    update() {
+        // left/right movement
+        if (keyLEFT.isDown) {
+            this.x -= this.moveSpeed;
+            this.y += this.moveSpeed;
+        }
+        else if (keyRIGHT.isDown) {
+            this.x += this.moveSpeed;
+            this.y -= this.moveSpeed;
+        }
+    }
+
+    reset() {
+        
+    }
+}
