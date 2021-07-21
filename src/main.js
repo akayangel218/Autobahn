aa3ca7051f0963bb29c1db416a14284af992947d
Mateen <maaminia@gmail.com>
2021-07-21 14:55:42 -0700

    turned off debug

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             debugShowVelocity: false,
             gravity: {
                 x: 0,
