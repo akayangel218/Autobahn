7f400006eda19b307c6d22d39a6d5b1f66031232
Mateen <maaminia@gmail.com>
2021-07-21 14:35:05 -0700

    updated instruct

--- src/scenes/Instruct.js
+++ src/scenes/Instruct.js
@@ -26,7 +26,7 @@ class Instruct extends Phaser.Scene {
         this.add.text(game.config.width / 2, game.config.height / 3, 'You must take your car on the autobahn to find', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2.5, ' the lost girl.', menuConfig).setOrigin(0.5);
 
-        this.add.text(game.config.width / 2, game.config.height / 2, 'Use all the arrow keys (<- and ->) to move through traffic,', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'Use the arrow keys (<- and ->) to move through traffic,', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 1.8, ' pull over to find the child', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 1.5, 'Be careful not to pick up any demons (they have a different bow)', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 1.4, 'Also, do not crash into other cars or go off the road', menuConfig).setOrigin(0.5);
