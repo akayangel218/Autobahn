12e2f138af84130d9d53d4017b54ce310020ff9c
Kidus Michael <kimichae@ucsc.edu>
2021-07-21 22:25:14 -0700

    space mods

--- src/scenes/Instruct.js
+++ src/scenes/Instruct.js
@@ -21,16 +21,16 @@ class Instruct extends Phaser.Scene {
         }
 
         // show instruction text
-        this.add.text(game.config.width / 2, game.config.height / 5, 'A little girl has gone missing and ', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 3.8, 'has been reported to have been seen on the Autobahn.', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 3, 'You must take your car on the autobahn to find', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 5, ' A little girl has gone missing and ', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 3.8, ' has been reported to have been seen on the Autobahn.', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 3, ' You must take your car on the autobahn to find ', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2.5, ' the lost girl.', menuConfig).setOrigin(0.5);
 
-        this.add.text(game.config.width / 2, game.config.height / 2, 'Use the arrow keys (<- and ->) to move through traffic,', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 1.8, ' pull over to find the child', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 1.5, 'Be careful not to pick up any demons (they have a different bow)', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 1.4, 'Also, do not crash into other cars or go off the road', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 1.2, 'Press S to start, Good Luck', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, ' Use the arrow keys (<- and ->) to move through traffic. ', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 1.8, ' Pull over to pick up the child. ', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 1.5, ' Be careful not to pick up any demons ', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 1.4, ' Also, do not crash into other cars or go off the road ', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 1.2, ' Press S to start, Good Luck ', menuConfig).setOrigin(0.5);
         //menuConfig.backgroundColor = '#00FF00';
         //menuConfig.color = '#000';
 
